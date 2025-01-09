e9dbb850902a0fe13ae5f279bf9f934163edfffc
soby <vthquddbsv@gmail.com>
2025-01-09 16:03:45 +0900

    first

---
+++ src/main/java/com/beyond/basic/b1_hello/domain/Hello.java
@@ -0,0 +1,14 @@
+package com.beyond.basic.b1_hello.domain;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor // 기본생성자
+@AllArgsConstructor // 모든매개변수있는 생성자
+//@Data : Getter, Setter, toString메서드 까지 포함하는 어노테이션
+public class Hello {
+    private String name;
+    @Setter // emai;변수에 관한 setter만 생성
+    private String email;
+    }
+
